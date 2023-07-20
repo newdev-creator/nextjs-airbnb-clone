8c78597ec3450c60d9a6feb23a4601ce2a7fab5a
Julien <newdevcreator@gmail.com>
2023-07-21 00:21:19 +0200

    FEAT: dropDown logged

--- app/actions/getCurrentUser.ts
+++ app/actions/getCurrentUser.ts
@@ -3,8 +3,30 @@ import { getServerSession } from "next-auth/next";
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
 import prisma from "@/app/libs/prismadb";
 
-export async function getSesion() {
+export async function getSession() {
   return await getServerSession(authOptions);
 }
 
-// TODO: 2:20:00
+export default async function getCurrentUser() {
+  try {
+    const session = await getSession();
+
+    if (!session?.user?.email) {
+      return null;
+    }
+
+    const currentUser = await prisma.user.findUnique({
+      where: {
+        email: session.user.email as string,
+      },
+    });
+
+    if (!currentUser) {
+      return null;
+    }
+
+    return currentUser;
+  } catch (error: any) {
+    return null;
+  }
+}
