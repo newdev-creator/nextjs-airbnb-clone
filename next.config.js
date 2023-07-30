f92d5b46f7058e65334ebfef863b9398e8c6dab3
Julien <newdevcreator@gmail.com>
2023-07-30 18:04:05 +0200

    FEAT: load image

--- next.config.js
+++ next.config.js
@@ -1,7 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ["avatar.githubusercontent.com", "lh3.googleusercontent.com"],
+    domains: [
+      "avatar.githubusercontent.com",
+      "lh3.googleusercontent.com",
+      "res.cloudinary.com",
+    ],
   },
 };
 
