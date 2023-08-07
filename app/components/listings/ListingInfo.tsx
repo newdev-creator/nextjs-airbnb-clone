254bffb686f5f82d61c8e8417df81e75f63673d6
Julien <newdevcreator@gmail.com>
2023-08-07 22:27:54 +0200

    FEAT: listingInfo

---
+++ app/components/listings/ListingInfo.tsx
@@ -0,0 +1,35 @@
+"use client";
+
+import { SafeUser } from "@/app/types";
+import { IconType } from "react-icons";
+
+interface ListingInfoProps {
+  user: SafeUser;
+  description: string;
+  guestCount: number;
+  roomCount: number;
+  bathroomCount: number;
+  category:
+    | {
+        icon: IconType;
+        label: string;
+        description: string;
+      }
+    | undefined;
+  locationValue: string;
+}
+
+const ListingInfo: React.FC<ListingInfoProps> = ({
+  user,
+  description,
+  guestCount,
+  roomCount,
+  bathroomCount,
+  category,
+  locationValue,
+}) => {
+  // 6:07:08
+  return <div></div>;
+};
+
+export default ListingInfo;
