b9fe16ea74919ba381d712ab0dc567e5bdde8ca7
Julien <newdevcreator@gmail.com>
2023-08-07 02:35:39 +0200

    FIX: SafeListing

--- app/page.tsx
+++ app/page.tsx
@@ -33,13 +33,14 @@ export default async function Home() {
             gap-8
           "
         >
-          {listings.map((listing: any) => {
+          {listings.map((listing) => {
             return (
               <ListingCard
                 currentUser={currentUser}
                 key={listing.id}
                 data={listing}
               />
+              // 5:43:26
             );
           })}
         </div>
