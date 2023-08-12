ad63b740a541d2b999f396653d9479b8d69cb9a6
Julien <newdevcreator@gmail.com>
2023-08-12 13:34:43 +0200

    FEAT: reservations

--- app/actions/getReservations.ts
+++ app/actions/getReservations.ts
@@ -7,41 +7,46 @@ interface IParams {
 }
 
 export default async function getReservations(params: IParams) {
-  const { listingId, userId, authorId } = params;
+  try {
+    const { listingId, userId, authorId } = params;
 
-  const query: any = {};
+    const query: any = {};
 
-  if (listingId) {
-    query.listingId = listingId;
+    if (listingId) {
+      query.listingId = listingId;
+    }
+
+    if (userId) {
+      query.userId = userId;
+    }
+
+    if (authorId) {
+      query.listing = { userId: authorId };
+    }
+
+    const reservations = await prisma.reservation.findMany({
+      where: query,
+      include: {
+        listing: true,
+      },
+      orderBy: {
+        createdAt: "desc",
+      },
+    });
+
+    const safeReservations = reservations.map((reservation) => ({
+      ...reservation,
+      createdAt: reservation.createdAt.toISOString(),
+      startDate: reservation.startDate.toISOString(),
+      endDate: reservation.endDate.toISOString(),
+      listing: {
+        ...reservation.listing,
+        createdAt: reservation.listing.createdAt.toISOString(),
+      },
+    }));
+
+    return safeReservations;
+  } catch (error: any) {
+    throw new Error(error);
   }
-
-  if (userId) {
-    query.userId = userId;
-  }
-
-  if (authorId) {
-    query.authorId = authorId;
-  }
-
-  const reservations = await prisma.reservation.findMany({
-    where: query,
-    include: {
-      listing: true,
-    },
-    orderBy: {
-      createdAt: "desc",
-    },
-  });
-
-  const safeReservations = reservations.map((reservation) => ({
-    ...reservation,
-    createdAt: reservation.createdAt.toISOString(),
-    startDate: reservation.startDate.toISOString(),
-    endDate: reservation.endDate.toISOString(),
-    listing: {
-      ...reservation.listing,
-      createdAt: reservation.listing.createdAt.toISOString(),
-    },
-  }));
-  // 6:50:47
 }
