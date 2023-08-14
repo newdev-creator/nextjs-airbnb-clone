ed983af5d9720a53ee98ecdf1e33b765114c60b4
Julien <newdevcreator@gmail.com>
2023-08-14 22:03:13 +0200

    FEAT: useSearchModal

---
+++ app/hooks/useSearchModal.ts
@@ -0,0 +1,15 @@
+import { create } from "zustand";
+//7:53:54
+interface LoginModalStore {
+  isOpen: boolean;
+  onOpen: () => void;
+  onClose: () => void;
+}
+
+const useLoginModal = create<LoginModalStore>((set) => ({
+  isOpen: false,
+  onOpen: () => set({ isOpen: true }),
+  onClose: () => set({ isOpen: false }),
+}));
+
+export default useLoginModal;
