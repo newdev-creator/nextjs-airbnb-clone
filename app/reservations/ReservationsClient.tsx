fcdfa402cf0fece414ad69423583bf08fb72b0c0
Julien <newdevcreator@gmail.com>
2023-08-14 15:14:23 +0200

    FEAT: reservationClient

---
+++ app/reservations/ReservationsClient.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { toast } from "react-hot-toast";
+import axios from "axios";
+import { useCallback, useState } from "react";
+import { useRouter } from "next/navigation";
+
+import { SafeReservation, SafeUser } from "../types";
+
+import Heading from "../components/Heading";
+import Container from "../components/Container";
+import ListingCard from "../components/listings/ListingCard";
+
+interface ReservationClientProps {
+  reservations: SafeReservation[];
+  currentUser?: SafeUser | null;
+}
+
+const ReservationsClient: React.FC<ReservationClientProps> = ({
+  reservations,
+  currentUser,
+}) => {
+  return (
+    <Container>
+      <Heading title="Reservations" subtitle="Bookings on your properties" />
+    </Container>
+  );
+};
+
+export default ReservationsClient;
