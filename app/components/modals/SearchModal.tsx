a078d0dc0cb9126c45dce761c6f3ee3c3d1552fb
Julien <newdevcreator@gmail.com>
2023-08-16 09:31:05 +0200

    FEAT: searchModal

---
+++ app/components/modals/SearchModal.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import { useRouter, useSearchParams } from "next/navigation";
+import Modal from "./Modal";
+
+import useSearchModal from "@/app/hooks/useSearchModal";
+import { useState } from "react";
+
+enum STEPS {
+  LOCATION = 0,
+  DATE = 1,
+  INFO = 2,
+}
+
+const SearchModal = () => {
+  const router = useRouter();
+  const params = useSearchParams();
+  const searchModal = useSearchModal();
+
+  const [step, setStep] = useState();
+  //7:57:55
+  return (
+    <Modal
+      isOpen={searchModal.isOpen}
+      onClose={searchModal.onClose}
+      onSubmit={searchModal.onOpen}
+      title="Filters"
+      actionLabel="Search"
+    />
+  );
+};
+
+export default SearchModal;
--- app/hooks/useSearchModal.ts
+++ app/hooks/useSearchModal.ts
@@ -1,15 +1,15 @@
 import { create } from "zustand";
-//7:53:54
-interface LoginModalStore {
+
+interface SearchModalStore {
   isOpen: boolean;
   onOpen: () => void;
   onClose: () => void;
 }
 
-const useLoginModal = create<LoginModalStore>((set) => ({
+const useSearchModal = create<SearchModalStore>((set) => ({
   isOpen: false,
   onOpen: () => set({ isOpen: true }),
   onClose: () => set({ isOpen: false }),
 }));
 
-export default useLoginModal;
+export default useSearchModal;
