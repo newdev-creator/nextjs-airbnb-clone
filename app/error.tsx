6e9f1042c77d23348c1bd66f42df257a28365168
Julien <newdevcreator@gmail.com>
2023-08-16 15:09:18 +0200

    FEAT: loader

---
+++ app/error.tsx
@@ -0,0 +1,13 @@
+"use client";
+
+import { useEffect } from "react";
+
+interface ErrorStateProps {
+  error: Error;
+}
+
+const ErrorState: React.FC<ErrorStateProps> = ({ error }) => {
+  //8:29:31
+};
+
+export default ErrorState;
