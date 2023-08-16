0a1aab68a26851ed55768b45bc145e6d5de632b8
Julien <newdevcreator@gmail.com>
2023-08-16 11:37:10 +0200

    FEAT: begin component

--- app/components/modals/SearchModal.tsx
+++ app/components/modals/SearchModal.tsx
@@ -1,10 +1,13 @@
 "use client";
 
+import { Range } from "react-date-range";
 import { useRouter, useSearchParams } from "next/navigation";
+import dynamic from "next/dynamic";
+
 import Modal from "./Modal";
 
 import useSearchModal from "@/app/hooks/useSearchModal";
-import { useState } from "react";
+import { useMemo, useState } from "react";
 
 enum STEPS {
   LOCATION = 0,
@@ -17,8 +20,24 @@ const SearchModal = () => {
   const params = useSearchParams();
   const searchModal = useSearchModal();
 
-  const [step, setStep] = useState();
-  //7:57:55
+  const [step, setStep] = useState(STEPS.LOCATION);
+  const [guestCount, setguestCount] = useState(1);
+  const [roomCount, setRoomCount] = useState(1);
+  const [bathroomCount, setBathroomCount] = useState(1);
+  const [dateRange, setDateRange] = useState<Range>({
+    startDate: new Date(),
+    endDate: new Date(),
+    key: "selection",
+  });
+
+  const Map = useMemo(
+    () =>
+      dynamic(() => import("../Map"), {
+        ssr: false,
+      }),
+    [location]
+  );
+  //7:59:57
   return (
     <Modal
       isOpen={searchModal.isOpen}
